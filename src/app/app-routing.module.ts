f13b9d838cccfa175a18115695fb8db3abf37b33
Your Name <rushikeshsunilganesh@gmail.com>
2019-02-25 23:52:33 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'aboutus', loadChildren: './aboutus/aboutus.module#AboutusPageModule' }
 ];
 
 @NgModule({
